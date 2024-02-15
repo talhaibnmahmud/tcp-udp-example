d3c3685ecb7e8fd103fcf2107716a9c29c7088e8
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-02-15 15:39:04 +0600

    Refactor TCP server code to handle client connections and events

--- src/tcp.ts
+++ src/tcp.ts
@@ -1,10 +1,57 @@
 import net from "node:net";
 
-const server = net.createServer((socket) => {
-	socket.write("Hello, world!\n");
-	socket.on("data", (data) => {
-		console.log(data.toString());
+const PORT = process.env.PORT ?? 8080;
+
+const server = net.createServer((connection) => {
+	// 'connection' listener
+	console.log("client connected");
+
+	connection.setEncoding("utf8");
+	connection.setKeepAlive(true, 15000);
+	connection.setTimeout(15000);
+	connection.setNoDelay(true);
+
+	connection.on("connect", () => {
+		console.log("client connected");
 	});
+
+	connection.on("ready", () => {
+		console.log("connection is ready to be used");
+	});
+
+	connection.on("data", (data) => {
+		console.log(`client says: ${data.toString()}`);
+		connection.write(`server received your message: ${data.toString()}`);
+	});
+
+	connection.on("timeout", () => {
+		console.log("client connection timeout");
+		connection.end();
+	});
+
+	connection.on("end", () => {
+		console.log("client disconnected");
+	});
+
+	connection.on("close", () => {
+		console.log("client closed connection");
+	});
+
+	connection.on("error", (err) => {
+		console.error(err);
+	});
+
+	connection.write("Hello from server!\r\n");
 });
 
-server.listen(8080);
+server.on("drop", () => {
+	console.log(`client dropped connection`);
+});
+
+server.on("error", (err) => {
+	console.error(err);
+});
+
+server.listen(PORT, () => {
+	console.log(`server listening on port ${PORT}`);
+});
