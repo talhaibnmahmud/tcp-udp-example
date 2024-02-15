69f578f04f7734f057b5b5e1ee3533191ea57b0b
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-02-15 15:54:41 +0600

    Refactor UDP server to use dynamic port

--- src/udp.ts
+++ src/udp.ts
@@ -1,11 +1,24 @@
 import dgram from "node:dgram";
 
+const PORT = parseInt(process.env.PORT ?? "8090");
 const controller = new AbortController();
+controller.signal.addEventListener("abort", () => {
+	console.log("server aborted");
+	server.close();
+});
+
 const server = dgram.createSocket({ type: "udp4", signal: controller.signal });
 server.on("message", (msg, rinfo) => {
 	console.log(
 		`server got: ${msg.toString()} from ${rinfo.address}:${rinfo.port} (${rinfo.family}) on ${rinfo.size} bytes`
 	);
+	server.send(
+		`server received your message: ${msg.toString()}`,
+		rinfo.port,
+		rinfo.address
+	);
 });
 
-server.bind(8090);
+server.bind(PORT, () => {
+	console.log(`server listening on port ${PORT}`);
+});
