0b35b476a6b91d9cdd0dfca4ece4c8159f09f351
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-07-08 15:14:51 +0600

    🧼 Update: .eslintrc.js formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es2021": true
-    },
-    "extends": ["standard-with-typescript", "plugin:prettier/recommended"],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-    }
-}
+	env: {
+		node: true,
+		browser: true,
+		es2021: true,
+	},
+	extends: ["standard-with-typescript", "plugin:prettier/recommended"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"@typescript-eslint/explicit-function-return-type": "off",
+	},
+};
